68fa4cf5e1e38acbf7376c3199ca01aaeee29156
ngagnidze <nikoloz_gagnidze@brown.edu>
2026-05-30 17:11:24 +0400

    send command implemented. Display on and off behavior

--- main/oled_driver.c
+++ main/oled_driver.c
@@ -5,10 +5,6 @@
 #define SDA_PIN 21
 #define SCL_PIN 22
 
-void app_main(void)
-{
-}
-
 void i2c_start()
 {
     gpio_set_direction(SDA_PIN, GPIO_MODE_OUTPUT);
@@ -44,7 +40,7 @@ void write_bit(int bit)
     gpio_set_level(SCL_PIN, 0);
 }
 
-void i2c_write_byte(u_int8_t byte)
+void i2c_write_byte(uint8_t byte)
 {
     for (int i = 7; i >= 0; i--)
     {
@@ -52,7 +48,7 @@ void i2c_write_byte(u_int8_t byte)
     }
 }
 
-void i2c_ack()
+int i2c_ack()
 {
     gpio_set_direction(SDA_PIN, GPIO_MODE_INPUT);
     ets_delay_us(5); // Allow the slave to pull the line and then read
@@ -60,4 +56,47 @@ void i2c_ack()
     int slave_signal = gpio_get_level(SDA_PIN);
     gpio_set_level(SCL_PIN, 0);
     return slave_signal;
+}
+
+void ssd1306_send_command(uint8_t cmd)
+{
+    // Initiate the communication
+    i2c_start();
+
+    // Call the address of the display. Set the mode to WRITE
+    i2c_write_byte(0x3C << 1 | 0);
+
+    if (i2c_ack() != 0)
+    {
+        printf("NACK on address. Exiting...");
+        i2c_stop();
+        return;
+    }
+
+    /*Following byte is a command.
+    Alternative: Following byte is data that you need to write into GDDRAM*/
+    i2c_write_byte(0x00);
+
+    if (i2c_ack() != 0)
+    {
+        printf("NACK on control byte. Exiting...");
+        i2c_stop();
+        return;
+    }
+
+    i2c_write_byte(cmd);
+
+    if (i2c_ack() != 0)
+    {
+        printf("NACK on command. Exiting...");
+        i2c_stop();
+    }
+    i2c_stop();
+}
+
+void app_main(void)
+{
+    ssd1306_send_command(0xAE);
+    ets_delay_us(50000);
+    ssd1306_send_command(0xAF);
 }
